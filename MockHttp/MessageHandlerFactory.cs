7019ac27e9c49a19a45d581f13006c88b94a4da2
Don Kackman <dkackman@gmail.com>
2015-03-25 17:51:30 -0500

    code commenting

--- MockHttp/MessageHandlerFactory.cs
+++ MockHttp/MessageHandlerFactory.cs
@@ -4,6 +4,10 @@ using System.Net.Http;
 
 namespace MockHttp
 {
+    /// <summary>
+    /// Flag indicating what type of <see cref="System.Net.Http.HttpMessageHandler"/> the 
+    /// <see cref="MockHttp.MessageHandlerFactory"/> will create by default
+    /// </summary>
     public enum MessageHandlerMode
     {
         Capture,
@@ -11,6 +15,10 @@ namespace MockHttp
         Online
     }
 
+    /// <summary>
+    /// Static factory class that creates <see cref="System.Net.Http.HttpMessageHandler"/>
+    /// instances for unit tests
+    /// </summary>
     public static class MessageHandlerFactory
     {
         static MessageHandlerFactory()
@@ -18,14 +26,29 @@ namespace MockHttp
             Mode = MessageHandlerMode.Online;
         }
 
+        /// <summary>
+        /// Controls what type of <see cref="System.Net.Http.HttpMessageHandler"/> to create by default
+        /// </summary>
         public static MessageHandlerMode Mode { get; set; }
 
+        /// <summary>
+        /// Create an <see cref="System.Net.Http.HttpMessageHandler"/>.
+        /// </summary>
+        /// <param name="mockResponseFolder">Path to the folder where mock response are stored (used when in Mock mode)</param>
+        /// <param name="captureFolder">Path to the folder to store captured responses (used when in capture mode)</param>
+        /// <returns>A <see cref="System.Net.Http.HttpMessageHandler"/></returns>
         public static HttpMessageHandler CreateMessageHandler(string mockResponseFolder, string captureFolder)
         {
             return CreateMessageHandler(mockResponseFolder, captureFolder, (key, value) => false);
         }
 
-
+        /// <summary>
+        /// Create an <see cref="System.Net.Http.HttpMessageHandler"/>.
+        /// </summary>
+        /// <param name="mockResponseFolder">Path to the folder where mock response are stored (used when in Mock mode)</param>
+        /// <param name="captureFolder">Path to the folder to store captured responses (used when in capture mode)</param>
+        /// <param name="paramFilter">A callback function that allows tests to filter query parameters out of serialization</param>
+        /// <returns>A <see cref="System.Net.Http.HttpMessageHandler"/></returns>
         public static HttpMessageHandler CreateMessageHandler(string mockResponseFolder, string captureFolder, Func<string, string, bool> paramFilter)
         {
             if (Mode == MessageHandlerMode.Mock)
