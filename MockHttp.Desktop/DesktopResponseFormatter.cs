0d38c83d1ddc9d232dd9f841b727d2c0044adc3f
Don Kackman <dkackman@gmail.com>
2015-03-28 15:06:16 -0500

    more portable support refactoring

---
+++ MockHttp.Desktop/DesktopResponseFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+using System.Security.Cryptography;
+
+namespace MockHttp.Desktop
+{
+    class DesktopResponseFormatter : RequestFormatter
+    {
+         public override string ToSha1Hash( string text) 
+         {
+             if (string.IsNullOrEmpty(text))
+             {
+                 return string.Empty;
+             }
+
+             using (var sha1 = new SHA1Managed()) 
+             { 
+                 byte[] textData = Encoding.UTF8.GetBytes(text); 
+                 byte[] hash = sha1.ComputeHash(textData);
+
+                 return BitConverter.ToString(hash).Replace("-", string.Empty); 
+             } 
+         }                 
+    }
+}
