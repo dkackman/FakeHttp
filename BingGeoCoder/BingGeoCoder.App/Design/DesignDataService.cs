cfc2363b08bc35652211800ca3b471a8bf12dcfb
Don Kackman <dkackman@gmail.com>
2015-05-05 20:46:27 -0500

    missing files

---
+++ BingGeoCoder/BingGeoCoder.App/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using BingGeoCoder.App.Model;
+
+namespace BingGeoCoder.App.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public Task<DataItem> GetData()
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            return Task.FromResult(item);
+        }
+    }
+}
---
+++ BingGeoCoder/BingGeoCoder.App/Model/IDataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BingGeoCoder.App.Model
+{
+    public interface IDataService
+    {
+        Task<DataItem> GetData();
+    }
+}
