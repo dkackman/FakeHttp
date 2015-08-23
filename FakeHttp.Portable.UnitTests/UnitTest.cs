786aaffacb150760d096d2b6247be314f717d044
Don Kackman <dkackman@gmail.com>
2015-08-23 10:00:35 -0500

    setting up for universal unit tests

---
+++ FakeHttp.Portable.UnitTests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace FakeHttp.Portable.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
